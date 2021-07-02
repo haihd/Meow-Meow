9a619e279b25d9d3474c6030f60fbca380b34733
Ha Hai <haihd@users.noreply.github.com>
2021-07-02 18:42:43 +0900

    Initial Commit

---
+++ Meow Meow/Meow_MeowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meow_MeowApp.swift
+//  Meow Meow
+//
+//  Created by Hai Ha on 2021/07/02.
+//
+
+import SwiftUI
+
+@main
+struct Meow_MeowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
